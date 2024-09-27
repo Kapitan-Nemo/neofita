4f9c2dff6b239322d78bc08794bd552cdd54a93f
Kapitan-Nemo <lasota020@gmail.com>
2024-09-27 23:35:02 +0200

    fix eslint warning for process

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,6 @@
+// Import the process module
+import process from 'node:process'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
