b5de756fba61def0165588418d63b956ed95d310
Kapitan-Nemo <lasota020@gmail.com>
2024-09-27 22:35:40 +0200

    upgrade of Date component behavior

--- components/Date.vue
+++ components/Date.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 const dateStore = useDateStore()
-const showDatePicker = ref(false)
 </script>
 
 <template>
@@ -8,12 +7,24 @@ const showDatePicker = ref(false)
     <p class="border-r py-2 px-6 text-sm border-gray-100 text-gray-200">
       Showing data
     </p>
-    <p class="text-gray-200 text-sm py-2 px-6 cursor-pointer gap-4 flex hover:text-red transition-all" @click="showDatePicker = !showDatePicker">
-      {{ dateStore.formatDate(dateStore.selectedDates.start) }} - {{ dateStore.formatDate(dateStore.selectedDates.end) }}
-      <Icon size="20" name="ion:calendar-sharp" />
-    </p>
-    <div class="absolute top-16 right-0">
-      <VDatePicker v-if="showDatePicker" v-model="dateStore.selectedDates" :style="{ backgroundColor: '#1e1e1e', border: '1px solid #454545' }" color="red" is-range is-dark />
-    </div>
+
+    <VDatePicker
+      v-model="dateStore.selectedDates"
+      expanded
+      :style="{ backgroundColor: '#1e1e1e', border: '1px solid #454545' }"
+      color="red"
+      is-range
+      is-dark
+    >
+      <template #default="{ togglePopover }">
+        <button
+          class="text-gray-200 text-sm py-2 px-6 cursor-pointer gap-4 flex hover:text-red transition-all"
+          @click="togglePopover"
+        >
+          {{ dateStore.formatDate(dateStore.selectedDates.start) }} - {{ dateStore.formatDate(dateStore.selectedDates.end) }}
+          <Icon size="20" name="ion:calendar-sharp" />
+        </button>
+      </template>
+    </VDatePicker>
   </div>
 </template>
