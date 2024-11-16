fe0df5d6343b836b2d2398c507eeff84188107f2
Kapitan-Nemo <lasota020@gmail.com>
2024-11-16 22:58:53 +0100

    Saving plan improve

--- pages/saving-plan.vue
+++ pages/saving-plan.vue
@@ -7,6 +7,17 @@ definePageMeta({
 <template>
   <div>
     <Nav class="border-b border-gray-100" />
-    <p>Saving plan</p>
+    <div class="py-6 flex flex-row justify-between items-center">
+      <div class="flex flex-col gap-1">
+        <h1>Saving plan</h1>
+        <p class="text-sm text-gray-200 flex items-center">
+          <NuxtLink to="/dashboard/">
+            Dashboard
+          </NuxtLink> <Icon class="mx-2" name="ion:chevron-forward-outline" /> <NuxtLink to="/saving-plan/">
+            Saving Plan
+          </NuxtLink>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
