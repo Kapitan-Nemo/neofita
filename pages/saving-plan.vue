9be6cec946d1c2e9f91108848f7e4d025880544e
Kapitan-Nemo <lasota020@gmail.com>
2024-11-16 22:55:40 +0100

    Saving plan view

---
+++ pages/saving-plan.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['auth'],
+})
+</script>
+
+<template>
+  <div>
+    <Nav class="border-b border-gray-100" />
+    <p>Saving plan</p>
+  </div>
+</template>
