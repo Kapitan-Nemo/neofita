8e8e4ba27981a2f69f5fcbcd5f6f70308d96e5b9
Kapitan-Nemo <lasota020@gmail.com>
2025-01-05 00:00:47 +0100

    Select default of whole current month instead of one week

--- stores/date.ts
+++ stores/date.ts
@@ -6,26 +6,10 @@ export const useDateStore = defineStore('dateStore', () => {
   const month = now.getMonth()
   const startOfMonth = new Date(year, month, 1)
   const endOfMonth = new Date(year, month + 1, 0, 23, 59, 59, 999)
-  const middleOfMonth1 = new Date(year, month, 12, 23, 59, 59, 999)
-  const middleOfMonth2 = new Date(year, month, 22, 23, 59, 59, 999)
-  let start, end
-  switch (true) {
-    case now <= middleOfMonth1:
-      start = startOfMonth
-      end = middleOfMonth1
-      break
-    case now <= middleOfMonth2:
-      start = new Date(year, month, 13)
-      end = middleOfMonth2
-      break
-    default:
-      start = new Date(year, month, 23)
-      end = endOfMonth
-      break
-  }
+
   const selectedDates = ref({
-    start,
-    end,
+    start: startOfMonth,
+    end: endOfMonth,
   })
 
   return {
