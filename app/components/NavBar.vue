132c5c8e417c55d26137e3d311d1a044f5d5ac2a
Kapitan-Nemo <lasota020@gmail.com>
2025-09-17 19:17:06 +0200

    Nav elements

---
+++ app/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav>
+        <ul class="flex space-x-4 p-4 bg-gray-800 text-white">
+            <li>
+                <NuxtLink to="/" class="hover:underline">Home</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/about-me" class="hover:underline">About Me</NuxtLink>
+            </li>
+        </ul>
+    </nav>
+</template>
